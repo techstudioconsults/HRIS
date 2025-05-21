e0be87db1934f547b5b1b67495639bc3f57b8dd1
Kingsley Ifijeh <kinxly@gmail.com>
2025-05-21 12:33:13 +0100

    fix: uncommented react-query dev tool

--- src/lib/react-query/query-provider.tsx
+++ src/lib/react-query/query-provider.tsx
@@ -8,7 +8,7 @@ export function ReactQueryProvider({ children }: { children: React.ReactNode }) 
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }

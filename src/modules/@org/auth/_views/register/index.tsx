1fc9d1f783d5af21e6bad72853fd848fd9883990
Kingsley Ifijeh <kinxly@gmail.com>
2025-05-24 11:35:16 +0100

    fead: created register screen

--- src/modules/@org/auth/_views/register/index.tsx
+++ src/modules/@org/auth/_views/register/index.tsx
@@ -6,7 +6,6 @@ import { FormField } from "@/components/shared/FormFields";
 import { Logo } from "@/components/shared/logo";
 import { RegisterFormData, registerSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Google } from "iconsax-reactjs";
 import Link from "next/link";
 import { FormProvider, useForm } from "react-hook-form";
 
@@ -108,29 +107,6 @@ export const Register = () => {
             </MainButton>
           </div>
         </form>
-
-        {/* <div className="relative my-6">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t"></div>
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="text-muted-foreground bg-white px-2">or continue with</span>
-          </div>
-        </div>
-
-        <MainButton
-          type="button"
-          isLeftIconVisible
-          icon={<Google />}
-          size="2xl"
-          variant="outline"
-          className="w-full"
-          // isDisabled={isGooglePending}
-          // isLoading={isGooglePending}
-          // onClick={handleGoogleSignIn}
-        >
-          Continue with Google
-        </MainButton> */}
 
         <p className="mt-6 text-center text-sm text-gray-500">
           Have an account already?{" "}

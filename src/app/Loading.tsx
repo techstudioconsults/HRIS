5183ed0eb54c2647355afcd6d54537c36c1ff605
Kingsley Ifijeh <kinxly@gmail.com>
2025-05-24 13:38:03 +0100

    feat: finshed auth design

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -2,7 +2,7 @@
 
 import { Logo } from "@/components/shared/logo";
 import { cn } from "@/lib/utils";
-import { LuLoader } from "react-icons/lu";
+import { LucideLoader } from "lucide-react";
 
 export default function Loading({ text, className }: { text?: string; className?: string }) {
   return (
@@ -10,7 +10,7 @@ export default function Loading({ text, className }: { text?: string; className?
       <div className="flex flex-col items-center gap-2">
         <Logo width={100} height={47} />
         <div className="flex items-center gap-1">
-          <LuLoader className="text-primary animate-spin text-xl" />
+          <LucideLoader className="text-primary animate-spin text-xl" />
           {<p className="text-sm font-medium">{text || "Loading..."}</p>}
         </div>
       </div>

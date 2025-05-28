88cffbf6b56065766081d1490da344ef633da10b
Kingsley Ifijeh <kinxly@gmail.com>
2025-05-28 09:24:29 +0100

    feat: SMS-19 completed onboarding ui

--- src/app/(public)/onboarding/page.tsx
+++ src/app/(public)/onboarding/page.tsx
@@ -1,14 +1,33 @@
-import { StepThree } from "@/modules/@org/onboarding";
+import { StepOne, StepThree, StepTwo, Welcome } from "@/modules/@org/onboarding";
 
-const page = () => {
-  return (
-    <>
-      {/* <Welcome /> */}
-      {/* <StepOne /> */}
-      {/* <StepTwo /> */}
-      <StepThree />
-    </>
-  );
+type OnboardingPageProperties = {
+  searchParams: {
+    step?: string;
+  };
 };
 
-export default page;
+const OnboardingPage = ({ searchParams }: OnboardingPageProperties) => {
+  // Determine the current step from query params, default to 'welcome'
+  const currentStep = searchParams.step || "welcome";
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case "1": {
+        return <StepOne />;
+      }
+      case "2": {
+        return <StepTwo />;
+      }
+      case "3": {
+        return <StepThree />;
+      }
+      default: {
+        return <Welcome />;
+      }
+    }
+  };
+
+  return <div className="onboarding-container">{renderStep()}</div>;
+};
+
+export default OnboardingPage;

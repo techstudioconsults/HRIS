b596a79b8ee027a6c4e4dc6e9ba76955dc278d23
i19n <kinxly@gmail.com>
2025-10-21 22:54:29 +0100

    fix: redesigned the teams permission view

--- src/modules/@org/admin/teams/_views/add-team/index.tsx
+++
@@ -1,9 +0,0 @@
-import { TeamForm } from "../../_components/forms/add-team";
-
-export const AddTeam = () => {
-  return (
-    <>
-      <TeamForm />
-    </>
-  );
-};
--- src/modules/@org/admin/teams/index.ts
+++ src/modules/@org/admin/teams/index.ts
@@ -1,4 +1,3 @@
 export { AllTeams } from "./_views/team";
-export { AddEmployee } from "./_views/add-team";
 export { TeamDetails } from "./_views/team-details";
 export { SubTeamDetails } from "./_views/sub-team-details";

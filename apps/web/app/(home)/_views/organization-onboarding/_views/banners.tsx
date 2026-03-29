f43dafec547185337f7f275cf38939aa5bf7c4fc
i19n <kinxly@gmail.com>
2026-03-29 16:32:56 +0100

    fix: smooth out accordion section

---
+++ apps/web/app/(home)/_views/organization-onboarding/_views/banners.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { BlurImage } from '@workspace/ui/components/core/miscellaneous/blur-image';
+import { Emphasis } from '../../../_components/Emphasis';
+import { Wrapper } from '@workspace/ui/components/core/layout/wrapper';
+import { MainButton } from '@workspace/ui/lib';
+
+export const TourBanner = () => {
+  return (
+    <Wrapper className="bg-primary/10 py-4 lg:py-[66px] lg:px-[58px]! my-0! rounded-[18px]">
+      <div className="flex justify-center flex-col-reverse md:flex-row items-center gap-5 lg:gap-10">
+        <div className="flex-1 space-y-10">
+          <header className={'space-y-4'}>
+            <h2
+              className="text-balance text-3xl font-semibold tracking-[-0.02em] text-zinc-800 md:text-4xl xl:text-[54px]
+       lg:leading-[1.2]"
+            >
+              Run <Emphasis>Payroll</Emphasis> Without the Chaos
+            </h2>
+            <p className="text-sm md:text-[15px] leading-normal text-zinc-500 lg:text-lg">
+              Automate salary processing, deductions, and payslips with a
+              payroll system built for modern African businesses.
+            </p>
+          </header>
+          <div
+            className="mx-auto flex w-full max-w-md flex-col items-center
+          gap-3 sm:max-w-none sm:flex-row sm:gap-4 lg:gap-5"
+          >
+            <MainButton variant={'primary'} className="w-full sm:w-auto">
+              Take Quick Tour
+            </MainButton>
+            <MainButton
+              variant={'primaryOutline'}
+              className="w-full bg-background sm:w-auto"
+            >
+              Login
+            </MainButton>
+          </div>
+        </div>
+        <div className="flex-1">
+          <BlurImage
+            src={
+              'https://res.cloudinary.com/kingsleysolomon/image/upload/f_auto,q_auto,w_1000/v1774717337/techstudio/hris-repo/fmimfi3rihq8clzj9gn2.webp'
+            }
+            alt={'tour-img'}
+            width={514}
+            height={469}
+            className={'border-4 rounded-xl border-background mx-auto'}
+          />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export const EmployeeBanner = () => {
+  return (
+    <Wrapper className="bg-primary/5 py-4 lg:py-[66px] lg:px-[58px]! my-0! rounded-[18px]">
+      <div className="flex justify-center flex-col-reverse md:flex-row-reverse items-center gap-5 lg:gap-10">
+        <div className="flex-1 space-y-10">
+          <header className={'space-y-4'}>
+            <h2
+              className="text-balance text-3xl font-semibold tracking-[-0.02em] text-zinc-800 md:text-4xl xl:text-[54px]
+       lg:leading-[1.2]"
+            >
+              A Better Experience for <Emphasis>Every Employee</Emphasis>
+            </h2>
+            <p className="text-sm md:text-[15px] text-balance leading-normal text-zinc-500 lg:text-lg">
+              Give your team easy access to payslips, attendance, and essential
+              HR tools, all in one seamless experience, wherever they work.
+            </p>
+          </header>
+          <div
+            className="mx-auto flex w-full max-w-md flex-col items-center
+          gap-3 sm:max-w-none sm:flex-row sm:gap-4 lg:gap-5"
+          >
+            <MainButton variant={'primary'} className="w-full sm:w-auto">
+              Start free Trial
+            </MainButton>
+            <MainButton
+              variant={'primaryOutline'}
+              className="w-full bg-background sm:w-auto"
+            >
+              Login
+            </MainButton>
+          </div>
+        </div>
+        <div className="flex-1">
+          <BlurImage
+            src={
+              'https://res.cloudinary.com/kingsleysolomon/image/upload/f_auto,q_auto,w_1000/v1774766845/techstudio/hris-repo/y6xgs9npgb9xvscim2fq.webp'
+            }
+            alt={'phones-img'}
+            width={514}
+            height={469}
+            className={'rounded-xl border-background mx-auto'}
+          />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
--- apps/web/app/(home)/_views/organization-onboarding/index.tsx
+++ apps/web/app/(home)/_views/organization-onboarding/index.tsx
@@ -1,6 +1,6 @@
 import { OrganizationOnboardingSectionHeader } from './_components/section-header';
 
-import { TourBanner } from './_views/tour-banner';
+import { EmployeeBanner, TourBanner } from './_views/banners';
 import { OnboardingStepper } from './_views/onboarding-stepper';
 import { Wrapper } from '@workspace/ui/components/core/layout/wrapper';
 
@@ -10,6 +10,7 @@ export const OrganizationOnboarding = () => {
       <OrganizationOnboardingSectionHeader />
       <OnboardingStepper />
       <TourBanner />
+      <EmployeeBanner />
     </Wrapper>
   );
 };

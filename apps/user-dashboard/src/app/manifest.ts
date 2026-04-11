da929a4ee031dcfae48efaa4ded723bb96dd8150
i19n <kinxly@gmail.com>
2026-04-11 23:02:36 +0100

    fix: resolved ios splash screen

--- apps/user-dashboard/src/app/manifest.ts
+++ apps/user-dashboard/src/app/manifest.ts
@@ -14,13 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ['productivity', 'business'],
     screenshots: [
       {
-        src: '/icons/icon-512.png',
+        src: '/icons/icon-512-v2.png',
         sizes: '512x512',
         type: 'image/png',
         form_factor: 'wide',
       },
       {
-        src: '/icons/icon-192.png',
+        src: '/icons/icon-192-v2.png',
         sizes: '192x192',
         type: 'image/png',
         form_factor: 'narrow',
@@ -28,19 +28,19 @@ export default function manifest(): MetadataRoute.Manifest {
     ],
     icons: [
       {
-        src: '/icons/icon-192.png',
+        src: '/icons/icon-192-v2.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/icons/icon-512.png',
+        src: '/icons/icon-512-v2.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/icons/icon-maskable-512.png',
+        src: '/icons/icon-maskable-512-v2.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
